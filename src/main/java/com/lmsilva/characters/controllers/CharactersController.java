50245b5bda02bae33422bc4483b73634b2f255eb
lmsilva-oss <62002634+lmsilva-oss@users.noreply.github.com>
2020-03-10 22:28:14 -0300

    Add other endpoint stubs

--- src/main/java/com/lmsilva/characters/controllers/CharactersController.java
+++ src/main/java/com/lmsilva/characters/controllers/CharactersController.java
@@ -2,9 +2,10 @@ package com.lmsilva.characters.controllers;
 
 import com.lmsilva.characters.swagger.ApiException;
 import com.lmsilva.characters.swagger.api.PublicApi;
-import com.lmsilva.characters.swagger.model.CharacterDataContainer;
+import com.lmsilva.characters.swagger.model.*;
 import org.apache.commons.codec.binary.Hex;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,25 +26,93 @@ public class CharactersController {
     @RequestMapping("/")
     // TODO: add query parameters
     public CharacterDataContainer get() throws ApiException, NoSuchAlgorithmException, UnsupportedEncodingException {
-        MessageDigest md5 = MessageDigest.getInstance("MD5");
-        long timestamp = System.currentTimeMillis()/1000; // ugly hack to fit into an int
-        // ugly hack to get an Integer
-        String ts = String.valueOf(timestamp);
-        Integer tsInt = Integer.parseInt(ts);
-
-        String hashBase = ts + PRIVATE_KEY + PUBLIC_KEY;
-        String hash = Hex.encodeHexString(md5.digest(hashBase.getBytes("UTF-8")));
+        MarvelAuth auth = new MarvelAuth(PUBLIC_KEY, PRIVATE_KEY);
 
         return apiClient
-                .getCreatorCollection(PUBLIC_KEY, hash, tsInt,
+                .getCreatorCollection(auth.getPublicKey(), auth.getHash(), auth.getTimestamp(),
                         null, null, null, null, null,
                         null, null, null, null, null)
                 .getData();
     }
 
-    // TODO: @RequestMapping('/v1/public/characters/{characterId}')
-    // TODO: @RequestMapping('/v1/public/characters/{characterId}/comics')
-    // TODO: @RequestMapping('/v1/public/characters/{characterId}/events')
-    // TODO: @RequestMapping('/v1/public/characters/{characterId}/series')
-    // TODO: @RequestMapping('/v1/public/characters/{characterId}/stories')
+    @RequestMapping("/v1/public/characters/{characterId}")
+    public CharacterDataContainer getCharacterIndividual(@PathVariable Integer characterId) throws NoSuchAlgorithmException, UnsupportedEncodingException, ApiException {
+        MarvelAuth auth = new MarvelAuth(PUBLIC_KEY, PRIVATE_KEY);
+
+        return apiClient
+                .getCharacterIndividual(characterId, auth.getPublicKey(), auth.getHash(), auth.getTimestamp())
+                .getData();
+    }
+
+    @RequestMapping("/v1/public/characters/{characterId}/comics")
+    public ComicDataContainer getCharacterComics(@PathVariable Integer characterId) throws ApiException, UnsupportedEncodingException, NoSuchAlgorithmException {
+        MarvelAuth auth = new MarvelAuth(PUBLIC_KEY, PRIVATE_KEY);
+        return apiClient.getComicsCharacterCollection(characterId, auth.getPublicKey(), auth.getHash(), auth.getTimestamp(),
+                null, null, null, null, null, null, null,
+        null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null,
+                null, null, null, null).getData();
+    }
+
+    @RequestMapping("/v1/public/characters/{characterId}/events")
+    public EventDataContainer getCharacterEvents(@PathVariable Integer characterId) throws UnsupportedEncodingException, NoSuchAlgorithmException, ApiException {
+        MarvelAuth auth = new MarvelAuth(PUBLIC_KEY, PRIVATE_KEY);
+
+        return apiClient
+                .getCharacterEventsCollection(characterId, auth.getPublicKey(), auth.getHash(), auth.getTimestamp(),
+                        null, null, null, null, null, null,
+                        null, null, null, null)
+                .getData();
+    }
+
+    @RequestMapping("/v1/public/characters/{characterId}/series")
+    public SeriesDataContainer getCharacterSeries(@PathVariable Integer characterId) throws UnsupportedEncodingException, NoSuchAlgorithmException, ApiException {
+        MarvelAuth auth = new MarvelAuth(PUBLIC_KEY, PRIVATE_KEY);
+
+        return apiClient
+                .getCharacterSeriesCollection(characterId, auth.getPublicKey(), auth.getHash(), auth.getTimestamp(),
+                        null, null, null, null, null, null,
+                        null, null, null, null, null, null, null)
+                .getData();
+    }
+
+    @RequestMapping("/v1/public/characters/{characterId}/stories")
+    public StoryDataContainer getCharacterStories(@PathVariable Integer characterId) throws UnsupportedEncodingException, NoSuchAlgorithmException, ApiException {
+        MarvelAuth auth = new MarvelAuth(PUBLIC_KEY, PRIVATE_KEY);
+
+        return apiClient
+                .getCharacterStoryCollection(characterId, auth.getPublicKey(), auth.getHash(), auth.getTimestamp(),
+                        null, null, null, null, null, null, null, null)
+                .getData();
+    }
+
+    private static class MarvelAuth {
+        private String hash;
+        private Integer timestamp;
+        private String publicKey;
+
+        public MarvelAuth(String publicKey, String privateKey) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            long timestamp = System.currentTimeMillis()/1000; // ugly hack to fit into an int
+            // ugly hack to get an Integer
+            String ts = String.valueOf(timestamp);
+            String hashBase = ts + privateKey + publicKey;
+
+            this.publicKey = publicKey;
+            this.timestamp = Integer.parseInt(ts);
+            this.hash = Hex.encodeHexString(md5.digest(hashBase.getBytes("UTF-8")));
+        }
+
+        public String getHash() {
+            return hash;
+        }
+
+        public Integer getTimestamp() {
+            return timestamp;
+        }
+
+        public String getPublicKey() {
+            return publicKey;
+        }
+    }
 }
