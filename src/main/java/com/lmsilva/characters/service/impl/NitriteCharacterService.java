190281e5718b2a1dce2905b8d65dc7a6439440c1
lmsilva-oss <62002634+lmsilva-oss@users.noreply.github.com>
2020-03-11 06:54:46 -0300

    Add characterId cross-reference to Nitrite service layer

--- src/main/java/com/lmsilva/characters/service/impl/NitriteCharacterService.java
+++ src/main/java/com/lmsilva/characters/service/impl/NitriteCharacterService.java
@@ -7,9 +7,14 @@ import org.dizitart.no2.Nitrite;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.dizitart.no2.objects.filters.ObjectFilters.eq;
+import static org.dizitart.no2.objects.filters.ObjectFilters.in;
 
 @Service(value="Nitrite")
 public class NitriteCharacterService implements ICharacterService {
@@ -39,7 +44,20 @@ public class NitriteCharacterService implements ICharacterService {
                                            List<Integer> events, List<Integer> stories,
                                            List<Integer> sharedAppearances, List<Integer> collaborators,
                                            List<String> orderBy, Integer limit, Integer offset) {
-        return db.getRepository(Comic.class).find().toList(); // TODO: add filtering and pagination
+        Character character = characterById(characterId);
+        List<ComicSummary> comics = character.getComics().getItems();
+        Set<Integer> ids = new HashSet<>();
+        Pattern pattern = Pattern.compile(".*/v1/public/comics/(\\d+)");
+        for (ComicSummary comic : comics) {
+            String baseUri = comic.getResourceURI();
+            Matcher matcher = pattern.matcher(baseUri);
+            if (!matcher.find()) {
+                throw new RuntimeException("Looks like Marvel changed their URL structure");
+            }
+            ids.add(Integer.valueOf(matcher.group(1)));
+        }
+        List<Comic> results = db.getRepository(Comic.class).find(in("id", ids.toArray())).toList();
+        return results; // TODO: add filtering and pagination
     }
 
     @Override
@@ -47,7 +65,20 @@ public class NitriteCharacterService implements ICharacterService {
                                            String modifiedSince, List<Integer> creators, List<Integer> series,
                                            List<Integer> comics, List<Integer> stories, List<String> orderBy,
                                            Integer limit, Integer offset) {
-        return db.getRepository(Event.class).find().toList(); // TODO: add filtering and pagination
+        Character character = characterById(characterId);
+        List<EventSummary> events = character.getEvents().getItems();
+        Set<Integer> ids = new HashSet<>();
+        Pattern pattern = Pattern.compile(".*/v1/public/events/(\\d+)");
+        for (EventSummary event : events) {
+            String baseUri = event.getResourceURI();
+            Matcher matcher = pattern.matcher(baseUri);
+            if (!matcher.find()) {
+                throw new RuntimeException("Looks like Marvel changed their URL structure");
+            }
+            ids.add(Integer.valueOf(matcher.group(1)));
+        }
+        List<Event> results = db.getRepository(Event.class).find(in("id", ids.toArray())).toList();
+        return results; // TODO: add filtering and pagination
     }
 
     @Override
@@ -56,13 +87,39 @@ public class NitriteCharacterService implements ICharacterService {
                                             List<Integer> stories, List<Integer> events, List<Integer> creators,
                                             String seriesType, List<String> contains, List<String> orderBy,
                                             Integer limit, Integer offset) {
-        return db.getRepository(Series.class).find().toList(); // TODO: add filtering and pagination
+        Character character = characterById(characterId);
+        List<SeriesSummary> seriesList = character.getSeries().getItems();
+        Set<Integer> ids = new HashSet<>();
+        Pattern pattern = Pattern.compile(".*/v1/public/series/(\\d+)");
+        for (SeriesSummary series : seriesList) {
+            String baseUri = series.getResourceURI();
+            Matcher matcher = pattern.matcher(baseUri);
+            if (!matcher.find()) {
+                throw new RuntimeException("Looks like Marvel changed their URL structure");
+            }
+            ids.add(Integer.valueOf(matcher.group(1)));
+        }
+        List<Series> results = db.getRepository(Series.class).find(in("id", ids.toArray())).toList();
+        return results; // TODO: add filtering and pagination
     }
 
     @Override
     public List<Story> storiesbyCharacterId(Integer characterId, String modifiedSince, List<Integer> comics,
                                             List<Integer> series, List<Integer> events, List<Integer> creators,
                                             List<String> orderBy, Integer limit, Integer offset) {
-        return db.getRepository(Story.class).find().toList(); // TODO: add filtering and pagination
+        Character character = characterById(characterId);
+        List<StorySummary> stories = character.getStories().getItems();
+        Set<Integer> ids = new HashSet<>();
+        Pattern pattern = Pattern.compile(".*/v1/public/stories/(\\d+)");
+        for (StorySummary story : stories) {
+            String baseUri = story.getResourceURI();
+            Matcher matcher = pattern.matcher(baseUri);
+            if (!matcher.find()) {
+                throw new RuntimeException("Looks like Marvel changed their URL structure");
+            }
+            ids.add(Integer.valueOf(matcher.group(1)));
+        }
+        List<Story> results = db.getRepository(Story.class).find(in("id", ids.toArray())).toList();
+        return results; // TODO: add filtering and pagination
     }
 }
